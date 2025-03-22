51df29eb3815912953b3a7ca864fed38222905a5
Rui <needfororigin11@gmail.com>
2025-03-22 22:37:40 +0300

    Shoes

--- Shoes/js/script.js
+++ Shoes/js/script.js
@@ -1,13 +1,41 @@
-const swiper = new Swiper('.swiper', {
+(function() {
 
-  slidesPerView: 1,
-  direction: 'vertical',
-  
-  
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+  // Первый слайдер //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 1,
+    direction: 'vertical',
     
-  },
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
+    },
+  
+  });
 
-});
+  // Бургер ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  document.addEventListener('click', burgerInit)
+
+  function burgerInit(event) {
+
+      const target = event.target
+      
+      const burgerIcon = target.closest('.burger-icon')
+      const burgerNavLink = target.closest('.header__link')
+      
+      if (!burgerIcon && !burgerNavLink) return
+      if (document.documentElement.clientWidth > 1100) return
+  
+      if (!document.body.classList.contains('body--opened-menu')) {
+          document.body.classList.add('body--opened-menu')
+   } else {
+      document.body.classList.remove('body--opened-menu')
+   }
+  }
+
+
+})()
