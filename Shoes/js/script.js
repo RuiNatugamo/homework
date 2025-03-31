ba1b8fc6ed445fcef0ae123e92f19cb27fe7422c
Rui <needfororigin11@gmail.com>
2025-03-31 18:58:56 +0300

    Shoes

--- Shoes/js/script.js
+++ Shoes/js/script.js
@@ -69,4 +69,28 @@
 
   });
 
+  // Аккордено  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const btnOpen = document.querySelector('.accordion__button-open')
+  const btnClose = document.querySelector('.accordion__button-close')
+  const accordionList = document.querySelector('.accordion-list')
+
+
+  const openAccordion = () => {
+    accordionList.classList.add('accordion--open')
+    btnClose.classList.add('show--btn')
+    btnOpen.classList.add('hide--btn')
+  }
+
+  const closeAccordion = () => {
+    accordionList.classList.remove('accordion--open')
+    btnClose.classList.remove('show--btn')
+    btnOpen.classList.remove('hide--btn')
+  }
+
+  btnOpen.addEventListener('click', openAccordion)
+
+  btnClose.addEventListener('click', closeAccordion)
+
+
 })()
